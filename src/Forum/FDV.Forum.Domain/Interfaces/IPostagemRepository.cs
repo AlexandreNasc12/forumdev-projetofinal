03818d3e7dec8321316b3203d8d544b553a36ea6
Alexandre <alexandre@techdog.com.br>
2024-08-29 21:38:57 -0300

    chore: implementando interface de repositórios

---
+++ src/Forum/FDV.Forum.Domain/Interfaces/IPostagemRepository.cs
@@ -0,0 +1,11 @@
+using System;
+using FDV.Core.Data;
+
+namespace FDV.Forum.Domain.Interfaces;
+
+public interface IPostagemRepository : IRepository<Postagem>, IDisposable
+{
+    Task<IEnumerable<Postagem>> ObterPublicadas();
+
+    Task<IEnumerable<Postagem>> ObterModeracao();
+}
